4e329943bbfae584adb3d142cc6cbf5d45b391c2
Katelyn Smith <katelyn.smith@uqconnect.edu.au>
2024-06-12 12:50:29 +0200

    ease enter the commit message for your changes. Lines starting

--- test_py_file.py
+++ test_py_file.py
@@ -2,3 +2,14 @@
 # why no conda environment :(((
 
 import numpy as np
+import matplotlib.pyplot as plt
+
+
+print("Hello World!")
+a = np.array([1, 2, 3])
+b = np.array([4, 5, 6])
+print(a)
+plt.plot(a, b)
+plt.savefig('test.png')
+plt.show()
+
